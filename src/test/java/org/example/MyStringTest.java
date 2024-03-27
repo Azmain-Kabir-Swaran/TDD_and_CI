82df2afbdcfcaa5a34c197e38861868324bc1bd0
Azmain Kabir <kabirswaran792@gmail.com>
2024-03-27 15:28:00 -0400

    Green: Test indexOfString for valid inputs

--- src/test/java/org/example/MyStringTest.java
+++ src/test/java/org/example/MyStringTest.java
@@ -6,9 +6,13 @@ import static org.junit.Assert.*;
 
 public class MyStringTest {
 
+    // Tests for valid inputs where the substring is found in the string
     @Test
-    public void indexOfString() {
+    public void testIndexOfStringWithValidInputs() {
         MyString myString = new MyString();
-        assertEquals("Expected return value should be 0", 0, myString.indexOfString("hello", "he", 0));
+        assertEquals("Substring 'he' found at start of 'hello'", 0, myString.indexOfString("hello", "he", 0));
+        assertEquals("Substring 'll' found in the middle of 'hello'", 2, myString.indexOfString("hello", "ll", 0));
+        assertEquals("Substring 'lo' found at the end of 'hello'", 3, myString.indexOfString("hello", "lo", 0));
+        assertEquals("Substring 'hello' equals the string 'hello'", 0, myString.indexOfString("hello", "hello", 0));
     }
 }
